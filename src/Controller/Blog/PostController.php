ff6b1ce595d34850a6026ee7da591eac53b07938
Louis Pinoit <louis@pinoit.com>
2023-11-05 03:11:36 +0100

    Second commit (pagination)

--- src/Controller/Blog/PostController.php
+++ src/Controller/Blog/PostController.php
@@ -3,23 +3,23 @@
 namespace App\Controller\Blog;
 
 use App\Repository\Post\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
     #[Route('/', name: 'post.index', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
+    public function index(
+        PostRepository $postRepository,
+        Request $request
+        ): Response
     {
 
-        $posts = $postRepository->findPublished();
-
-        
-
-
         return $this->render('pages/blog/index.html.twig', [
-            'posts' => $posts,
+            'posts' => $postRepository->findPublished($request->query->getInt('page', 1)),
         ]);
     }
 }
